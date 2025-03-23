6b1a32ec2c1efd1ca7eccb6a0981e72342986d5d
kooogi <kooogitv@gmail.com>
2025-03-23 12:56:59 +0100

    fix error with MovieLens in CF

--- ppera/datasets_loader.py
+++ ppera/datasets_loader.py
@@ -129,8 +129,9 @@ class MovieLensDataset(BaseDatasetLoader):
 
 
       final_merge_file_df['genres'] = final_merge_file_df['genres'].str.replace('|', ' ', regex=False)
-    #   final_merge_file_df['timestamp'] = pd.to_datetime(final_merge_file_df['timestamp'])
-    #   final_merge_file_df['timestamp'] = final_merge_file_df['timestamp'].astype('int64') // 10**9
+
+      final_merge_file_df['timestamp'] = pd.to_datetime(final_merge_file_df['timestamp'])
+      final_merge_file_df['timestamp'] = final_merge_file_df['timestamp'].astype('int64') // 10**9
 
       final_merge_file_df = final_merge_file_df.drop_duplicates(subset=['userID', 'itemID', 'rating'], keep='first')
     # drop unnecessary columns
