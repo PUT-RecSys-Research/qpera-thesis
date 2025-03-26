6f1c2cb38b01e72dac6b6c329461500ab1b26088
kooogi <kooogitv@gmail.com>
2025-03-26 18:05:51 +0100

    add cf_bpr to repo and metrics

--- ppera/main.py
+++ ppera/main.py
@@ -1,29 +1,36 @@
 import CF
 import CBF
+import CF_BPR
 
 # mlflow server --host 127.0.0.1 --port 8080
 # remember to be in the right directory to run this script from the command line (cd ppera)
 
-CF.cf_experiment_loop(TOP_K=10, dataset='movielens',
+# CF.cf_experiment_loop(TOP_K=10, dataset='movielens',
+#                         want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
+#                         num_rows=10000,
+#                         ratio=0.75,
+#                         seed=42)
+
+# CBF.cbf_experiment_loop(TOP_K=10, dataset='movielens',
+#                         want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
+#                         num_rows=10000,
+#                         ratio=0.75,
+#                         seed=42)
+
+# CF.cf_experiment_loop(TOP_K=10, dataset='amazonsales',
+#                         want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
+#                         num_rows=1000,
+#                         ratio=0.75,
+#                         seed=42)
+
+# CBF.cbf_experiment_loop(TOP_K=10, dataset='amazonsales',
+#                         want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
+#                         num_rows=1000,
+#                         ratio=0.75,
+#                         seed=42)
+
+CF_BPR.cf_bpr_experiment_loop(TOP_K=10, dataset='movielens',
                         want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
                         num_rows=10000,
-                        ratio=0.75,
-                        seed=42)
-
-CBF.cbf_experiment_loop(TOP_K=10, dataset='movielens',
-                        want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
-                        num_rows=10000,
-                        ratio=0.75,
-                        seed=42)
-
-CF.cf_experiment_loop(TOP_K=10, dataset='amazonsales',
-                        want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
-                        num_rows=1000,
-                        ratio=0.75,
-                        seed=42)
-
-CBF.cbf_experiment_loop(TOP_K=10, dataset='amazonsales',
-                        want_col=["userID", "itemID", "rating", "timestamp", 'title', 'genres'],
-                        num_rows=1000,
                         ratio=0.75,
                         seed=42)
